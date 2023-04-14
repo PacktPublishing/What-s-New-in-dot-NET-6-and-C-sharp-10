317d371985776ed8c5a0414495818aec4abcfcd6
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2023-04-14 23:30:48 +0530

    Updated

---
+++ Section 3 - Web API Project/After/Cms.Data.Repository/Repositories/ICmsRepository.cs
@@ -0,0 +1,20 @@
+
+namespace Cms.Data.Repository.Repositories;
+
+public interface ICmsRepository
+{
+    // Collection
+    IEnumerable<Course> GetAllCourses();
+    Task<IEnumerable<Course>> GetAllCoursesAsync();
+    Course AddCourse(Course newCourse);
+    bool IsCourseExists(int courseId);
+
+    // Individual item
+    Course GetCourse(int courseId);
+    Course UpdateCourse(int courseId, Course newCourse);
+    Course DeleteCourse(int courseId);
+
+    // Association 
+    IEnumerable<Student> GetStudents(int courseId);
+    Student AddStudent(Student student);
+}
---
+++ Section 3 - Web API Project/After/Cms.WebApi/GlobalUsings.cs
@@ -0,0 +1,18 @@
+
+global using System.Collections.Generic;
+global using System.Linq;
+global using Cms.Data.Repository.Repositories;
+global using Cms.WebApi.Mappers;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Cms.Data.Repository.Models;
+global using Cms.WebApi.DTOs;
+global using System.ComponentModel.DataAnnotations;
+global using System.Text.Json.Serialization;
+global using Microsoft.AspNetCore.Http;
+global using AutoMapper;
+global using Microsoft.OpenApi.Models;
